4140dcc5e61e1c9543410d3e57867d06923d965a
LittleZabi_ <79798080+LittleZabi@users.noreply.github.com>
2023-04-04 16:39:23 +0500

    Update database.php

--- admin/database.php
+++ admin/database.php
@@ -1,10 +1,7 @@
 <?php
 session_start();
-// $_SESSION['user']['username'] = 'admin';
-// $_SESSION['user']['email'] = 'mtcomputers.tkw@gmail.com';
-// $_SESSION['user']['fullname'] = 'System Administrator';
 if ($_SERVER['REMOTE_ADDR'] == '::1') {
     $con = mysqli_connect('localhost', 'root', '', 'gbfirmw1_res');
 } else {
-    $con = mysqli_connect('localhost', 'gbfirmw1_res', 'Abbas786@786@', 'gbfirmw1_res');
+    $con = mysqli_connect('localhost', '', '', '');
 }
